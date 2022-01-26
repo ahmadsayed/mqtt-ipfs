e8145ee9a62e759d12ac09212fd8fb37dfa376e1
Hassan Ahmed <ahmed@hassans-air.lan>
2022-01-27 01:31:59 +0800

    mqtt-ipfs-second-take

--- index.js
+++ index.js
@@ -1,30 +1,72 @@
 const cluster = require('cluster')
-const mqemitter = require('./libs/mqemitter-ipfs.js')
+
+const ipfs = require('ipfs')
+const ipfsClient = require('ipfs-http-client')
 const crypto = require('crypto')
 
 
+ipfs_topic = null;
+ipfs_encryption_key = null;
+cipher = null;
+
+const bridgeId = crypto.randomUUID();
+
+const ipfs_client = ipfsClient.create({
+  host: "localhost", port: "1880", protocol: 'http'
+})
+
+var hmac = null;
 function startAedes (ipfs_host, ipfs_port, mqtt_port) {
   const port = mqtt_port || 1883
   let hmac = null;
   const aedes = require('aedes')({
     authenticate: (client, username, password, done) => {
       console.log("authenticated");
-      hmac = crypto.createHmac('sha256', password).update(username).digest('hex');
-      console.log(hmac);
-      aedes.publish({ topic: 'aedes/hello/hmac', payload: hmac })
+      ipfs_topic = crypto.createHmac('sha256', password)
+        .update(username)
+        .digest('hex');
+
+      ipfs_encryption_key =  crypto.createHmac('sha256', username)
+        .update(password)
+        .digest('hex');
+      cipher = crypto.createCipher('aes192', ipfs_encryption_key);
+
+      ipfs_client.pubsub.subscribe(ipfs_topic, function (msg) {
+        console.log(`subscribed to ${ipfs_topic}`);
+        //Message recieve from remote queue or current queue
+        // if message received from remote queue -> publish it here
+        // if message recieve from current queue -> ignore
+        ipfs_message = JSON.parse(new TextDecoder("utf-8").decode(msg.data));
+        console.log(ipfs_message.message.data);
+        if (ipfs_message.bridgeId !== bridgeId) {
+          console.log("Recieved message from peer broker");
+          console.log("Informing current broker: " + ipfs_message.topic);
+          console.log(ipfs_message.message.data);
+          buffer = new TextDecoder("utf-8").decode(Buffer.from(ipfs_message.message.data));
+          console.log(buffer);
+          console.log('-------------------');
+          message = Buffer.from(buffer.split('.')[0]).toString('ascii');
+          console.log(message);
+          // Publish to Internal topic, this topic will not be propgated to IPFS
+          aedes.publish(ipfs_message.topic, Buffer.from(message, 'base64'));
+        }
+        console.log(new TextDecoder("utf-8").decode(msg.data));
+      })
+
+      console.log(ipfs_topic);
+      console.log(ipfs_encryption_key);
       done(null, true)
     },
-    id: 'IPFS_BROKER',
-    mq: mqemitter({
-      host: ipfs_host, //'cluster.provider-2.prod.ewr1.akash.pub',
-      port: ipfs_port  //'31105'
-    })
-  })
+    id: 'IPFS_BROKER'
+  });
+
+
+
   const server = require('net').createServer(aedes.handle);
 
   server.listen(port, function () {
     console.log('Aedes listening on port:', port)
-    aedes.publish({ topic: 'aedes/hello', payload: "I'm broker " + aedes.id })
+    //aedes.publish({ topic: 'aedes/hello', payload: "I'm broker " + aedes.id })
   })
 
   aedes.on('subscribe', function (subscriptions, client) {
@@ -49,7 +91,30 @@ function startAedes (ipfs_host, ipfs_port, mqtt_port) {
 
   // fired when a message is published
   aedes.on('publish', async function (packet, client) {
-    console.log('Client \x1b[31m' + (client ? client.id : 'BROKER_' + aedes.id) + '\x1b[0m has published', packet.payload.toString(), 'on', packet.topic, 'to broker', aedes.id)
+    console.log("$$$$$$");
+    console.log(client);
+
+    if (client == null) {
+      console.log("Do not publish to IPFS");
+
+    } else {
+      base64_message = Buffer.from(packet.payload).toString('base64');
+      hashed_base64_message = crypto.createHash('sha256').update(base64_message).digest('hex');
+      signature = cipher.update(hashed_base64_message,  'utf8', 'hex');
+      ipfs_signed_message = base64_message + "." + signature;
+
+      packet = {
+        topic: packet.topic,
+        message: Buffer.from(ipfs_signed_message),
+        bridgeId: bridgeId
+      }
+      // recieve the message and publsih it on ipfs pubsub ipfs_topic
+      // ipfs_topic is constructed based on the username
+      // and password using to authenticate to mqtt for simplicity,
+      // a more sophisticated use case can be established
+      ipfs_client.pubsub.publish(ipfs_topic, Buffer.from(JSON.stringify(packet)));
+      //console.log('Client \x1b[31m' + (client ? client.id : 'BROKER_' + aedes.id) + '\x1b[0m has published', packet.payload.toString(), 'on', packet.topic, 'to broker', aedes.id)
+    }
   })
 }
 
